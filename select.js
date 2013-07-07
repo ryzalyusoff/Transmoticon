6cdd1802d326851bd440ada041fa3b4f6d5035e4
Arief Fikhrie <arief.fik91@gmail.com>
2013-07-07 12:44:23 +0800

    latest

--- select.js
+++ select.js
@@ -115,33 +115,19 @@ $(document).ready(function(){
 	$('body').append(div);
 	
 	$(document).bind("mouseup",function(event){
-		var data = textReader(getSelected());
-		$('#pop').css("display","none")
-		if ( data.length !== 0 ){
-			$('#pop').html(
+		var keyword = String(getSelected());
+		$('#pop').css("display","none");
+		$.post('http://fikhrie.net/transmoticon/',{q : keyword }, function(receive) {
+			//console.log(receive);
+			/*if ( receive.image != "" ){
+				receive.image = "<img src='" + receive.image + "' />";
+			}*/
+			$('#pop').css("display","block").css("webkit-border-radius","5px").css("font-family","arial,sans-serif").css("background-color","#eeeeee").css("font-size","12px").css("padding","10px").css("border","1px solid #727272").css("top",event.pageY).css("left",event.pageX).html(
+				"<div style='width:240px;height:25px;border-bottom:1px solid #727272;margin-bottom:10px;' >" + receive.image + " " + receive.meaning + "</div>"
 			
-				"<div style='width:240px;height:25px;border-bottom:1px solid #727272;margin-bottom:10px;' >" + data[3] + " " + data[1] + "</div>"
-			
-				 + data[2]
+				 + receive.description
 			
 			);
-			
-			//parseHTML(data[3]);
-		}
-		//console.log(getSelected()+"");
+		}, "json");
 	});
-
-	
-	
-	
-
 });
-
-function textReader(text){
-	for ( var i = 0; i < demoData.length; i++ ){
-		if ( demoData[i][0] === String(text).toLowerCase() ){
-			return demoData[i];
-		}
-	}
-	return [];
-}
